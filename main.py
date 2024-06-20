41d3bfe3a9c38d80febae09f3202ceff675440cb
REFLECTIONS\kevin.tl <kevin.tl@reflectionsinfos.com>
2024-06-20 18:31:10 +0530

    deployment test

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 import uvicorn
 from fastapi import FastAPI
 from routers.say_hello import router_say_hello
@@ -7,4 +8,6 @@ app = FastAPI()
 app.include_router(router_say_hello)
 
 if __name__ == '__main__':
-    uvicorn.run('main:app', host="0.0.0.0", port=8000, reload=False)
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run('main:app', host="0.0.0.0", port=port, reload=False)
+
--- routers/say_hello.py
+++ routers/say_hello.py
@@ -6,3 +6,8 @@ router_say_hello = APIRouter(prefix='/say')
 @router_say_hello.get('/hello')
 async def say_hello():
     return {"message": "Hello"}
+
+
+@router_say_hello.get('/welcome')
+async def say_welcome():
+    return {"message": "Welcome"}
