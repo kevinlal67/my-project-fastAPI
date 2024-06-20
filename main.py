9b23ba843a42d4ed81889e272fbf854260babb7a
REFLECTIONS\kevin.tl <kevin.tl@reflectionsinfos.com>
2024-06-20 18:44:42 +0530

    deployment test

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
 import os
 import uvicorn
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
+
 from routers.say_hello import router_say_hello
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:3000",  # Your React app's URL
+    "https://my-project-fastapi.onrender.com"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(router_say_hello)
 
